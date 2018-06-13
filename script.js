a8320cf5e324bbc3843ebeef89f444aabba7da17
Michael J. Lyons <mikelyons7@me.com>
2018-06-13 09:18:33 -0600

    Add paddle to game

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth/2);
+var rightPressed = false;
+var leftPressed = false;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
 function drawBall() {
   ctx.beginPath();
@@ -14,19 +22,53 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
 
-  if (x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
   }
-  if (y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
     dy = -dy;
   }
-  
+
   x += dx;
   y += dy;
+
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+  }
+  else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
+}
+
+function keyDownHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = true;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+
+function keyUpHandler(e) {
+  if(e.keyCode == 39) {
+    rightPressed = false;
+  }
+  else if(e.keyCode == 37) {
+    leftPressed = false;
+  }
 }
 
 setInterval(draw, 10);
