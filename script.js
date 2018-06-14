22ee245eedbf45f22bfa39c2715ab11d2218c639
Michael J. Lyons <mikelyons7@me.com>
2018-06-14 13:44:45 -0600

    Add score counter and scoreboard

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var brickHeight = 20;
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
+var score = 0;
 
 var bricks = [];
 for(var c = 0; c < brickColumnCount; c++) {
@@ -68,6 +69,7 @@ function draw() {
   drawBall();
   drawBricks();
   drawPaddle();
+  drawScore();
   collisionDetection()
 
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -123,10 +125,21 @@ function collisionDetection() {
         if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
           dy = -dy;
           b.status = 0;
+          score++;
+          if(score == brickRowCount*brickColumnCount) {
+            alert("YOU WIN, LUCKY BASTARD");
+            document.location.reload();
+          }
         }
       }
     }
   }
+}
+
+function drawScore() {
+  ctx.font = "16px Sans";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: "+ score, 8, 20);
 }
 
 setInterval(draw, 10);
