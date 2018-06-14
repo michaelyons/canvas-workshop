a2c4b3ef9ff08fc8abdd1c39a54bfd1c79ad0b78
Michael J. Lyons <mikelyons7@me.com>
2018-06-13 20:21:25 -0600

    Add for loop for bricks

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0};
+  }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
